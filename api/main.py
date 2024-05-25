101a049cdd9a2bcfb9fd6b24f599d11e51c79472
m0onsoo <moonsoo2008@gmail.com>
2024-05-25 23:48:14 +0900

    feat: 커플 추천 api 구현
    user1과 user2의 id를 받아 추천받은 식당의 리스트를 반환

--- api/main.py
+++ api/main.py
@@ -12,65 +12,84 @@ import os
 # 현재 파일(main.py)의 디렉토리 경로를 가져옵니다.
 current_dir = os.path.dirname(__file__)
 
+# 'Data' 폴더 경로 설정
+data_folder = os.path.join(current_dir, '..', 'Data')
+
+# 유저/레스토랑 임베딩 로드
+user_path = os.path.join(data_folder, 'user_embedding.csv')
+user_embedding = pd.read_csv(user_path, index_col='user_id')
+
+restaurant_path = os.path.join(data_folder, 'user_embedding.csv')
+restaurant_embedding = pd.read_csv(restaurant_path, index_col="user_id")
+
+# string 형식을 python list로 변환
+user_embedding['embedding'] = user_embedding['embedding'].apply(ast.literal_eval)
+restaurant_embedding["embedding"] = restaurant_embedding["embedding"].apply(ast.literal_eval)
+
+try:
+    user_embedding.drop(columns='Unnamed: 0',inplace=True)
+    restaurant_embedding.drop(columns='Unnamed: 0',inplace=True)
+except:
+    pass
+
+# 활성 함수 정의
+activate_f = nn.Sigmoid()
+
+# 전체 레스토랑에 대한 임베딩 텐서화
+embeddings_list = restaurant_embedding['embedding'].tolist()
+embeddings_tensor = torch.tensor(embeddings_list)
+
 app = FastAPI()
 
+# Default Screen
 @app.get("/")
 def root():
     return {"Default": "Hello World-!"}
 
-
+# 단일 유저 추천
 @app.get("/recommend")
-def recommend(user: int):
-    # 'Data' 폴더와 'user_embedding.csv' 파일의 경로를 설정합니다.
-    data_folder = os.path.join(current_dir, '..', 'Data')
-
-    # 파일 경로 출력 (확인용)
-    print(f"File path: {data_folder}")
-
-
-    # 단일 유저 추천 결과
-    user_path = os.path.join(data_folder, 'user_embedding.csv')
-    u_emb = pd.read_csv(user_path, index_col='user_id')
-
-    restaurant_path = os.path.join(data_folder, 'user_embedding.csv')
-    r_emb = pd.read_csv(restaurant_path, index_col="user_id")
-
-    # string 형식을 python list로 변환
-    u_emb['embedding']=u_emb['embedding'].apply(ast.literal_eval)
-    r_emb["embedding"] = r_emb["embedding"].apply(ast.literal_eval)
-
-    try:
-        u_emb.drop(columns='Unnamed: 0',inplace=True)
-        r_emb.drop(columns='Unnamed: 0',inplace=True)
-    except:
-        pass
-
-    activate_f=nn.Sigmoid()
-    # 전체 레스토랑에 대한 임베딩 텐서화
-    tmp=[]
-    for i in range(len(r_emb)):
-        tmp.append(r_emb.loc[i,'embedding'])
-    rst_embedding_matrix=torch.tensor(tmp)
-
-
-
-
-    user_embedding_vector=torch.tensor(u_emb.loc[user,'embedding'])
+async def recommend(user: int):
+    # 유저 임베딩 텐서화
+    user_embedding_vector = torch.tensor(user_embedding.loc[user,'embedding'])
     
-    # user = request.files['user']
-    one_user_predict=activate_f(torch.matmul(user_embedding_vector,rst_embedding_matrix.t()))
-    _, one_user_rating=torch.topk(one_user_predict,k=100)
-    result_user = np.array(one_user_rating).tolist()
+    # 단일 유저 추론
+    one_user_predict = activate_f(torch.matmul(user_embedding_vector,embeddings_tensor.t()))
+    _, one_user_rating = torch.topk(one_user_predict, k=100)
+    result_restaurant_list = np.array(one_user_rating).tolist()
 
     result_dict = {
-        'user_num':user,
-        'result':result_user
+        "user_num": user,
+        "result": result_restaurant_list
     }
-
 
     return json.dumps(result_dict)
 
 
+# 커플 유저 추천
+@app.get("/recommend/couple")
+async def recommend_couple(user1: int, user2: int):
+    # 각 유저의 임베딩 텐서화
+    user1_embedding_vector = torch.tensor(user_embedding.loc[user1,'embedding'])
+    user2_embedding_vector = torch.tensor(user_embedding.loc[user1,'embedding'])
 
+    # 커플 임베딩 생성
+    couple_embedding_vector = torch.mul(user1_embedding_vector, user2_embedding_vector)
+    
+    # 커플 유저 결과 추론
+    couple_user_predict = activate_f(torch.matmul(couple_embedding_vector, embeddings_tensor.t()))
+    _, couple_user_rating = torch.topk(couple_user_predict,k=100)
+    result_restaurant_list = np.array(couple_user_rating).tolist()
+
+    # 결과값 딕셔너리화
+    result_dict = {
+        "user1": user1,
+        "user2": user2,
+        "result": result_restaurant_list
+    }
+
+    return result_dict
+
+
+# main
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
