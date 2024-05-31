00582542dac5829d7e26d908917314f36b083a1b
m0onsoo <moonsoo2008@gmail.com>
2024-05-31 21:19:13 +0900

    feat: district 추가

--- api/functions.py
+++ api/functions.py
@@ -2,25 +2,19 @@ import os
 import pymysql
 import pandas as pd
 import ast
+import torch
 from dotenv import  load_dotenv
 
 
 load_dotenv()  # .env 파일을 불러와 환경 변수 설정
 
-"""
-MODEL_HOST = 'capstone-db.c9mguk040cbf.us-east-1.rds.amazonaws.com'
+
+MODEL_HOST = 'palette-db.c10es6yky6c8.ap-northeast-2.rds.amazonaws.com'
 MODEL_PORT = 3306
 MODEL_USER = 'model'
 MODEL_PW = '1234'
 DB = 'palette'
-"""
 
-# 환경 변수에서 DB 접근 정보 가져오기
-MODEL_HOST = os.getenv('MODEL_HOST')
-MODEL_PORT = int(os.getenv('MODEL_PORT'))
-MODEL_USER = os.getenv('MODEL_USER')
-MODEL_PW = os.getenv('MODEL_PW')
-DB = os.getenv('DB')
 
 # SQL Database에 연결
 def DB_CONNECT():
@@ -48,12 +42,24 @@ def DATA_LOADER():
     user_path = os.path.join(data_folder, 'user_embedding.csv')
     user_embedding = pd.read_csv(user_path, index_col='user_id')
 
+    # 광진 레스토랑 임베딩 로드
     restaurant_path = os.path.join(data_folder, 'rst_embedding.csv')
-    restaurant_embedding = pd.read_csv(restaurant_path, index_col="user_id")
+    restaurant_embedding_KJ = pd.read_csv(restaurant_path, index_col="user_id")
+
+    # 홍대 레스토랑 임베딩 로드
+    restaurant_path = os.path.join(data_folder, 'rst_embedding.csv')
+    restaurant_embedding_HD = pd.read_csv(restaurant_path, index_col="user_id")
+
+    # 잠실 레스토랑 임베딩 로드
+    restaurant_path = os.path.join(data_folder, 'rst_embedding.csv')
+    restaurant_embedding_JS = pd.read_csv(restaurant_path, index_col="user_id")
 
     # string 형식을 python list로 변환
     user_embedding['embedding'] = user_embedding['embedding'].apply(ast.literal_eval)
-    restaurant_embedding["embedding"] = restaurant_embedding["embedding"].apply(ast.literal_eval)
+    restaurant_embedding_KJ["embedding"] = restaurant_embedding_KJ["embedding"].apply(ast.literal_eval)
+    restaurant_embedding_HD["embedding"] = restaurant_embedding_HD["embedding"].apply(ast.literal_eval)
+    restaurant_embedding_JS["embedding"] = restaurant_embedding_JS["embedding"].apply(ast.literal_eval)
+
 
     try:
         user_embedding.drop(columns='Unnamed: 0',inplace=True)
@@ -61,4 +67,4 @@ def DATA_LOADER():
     except:
         pass
 
-    return user_embedding, restaurant_embedding
+    return user_embedding, restaurant_embedding_KJ, restaurant_embedding_HD, restaurant_embedding_JS
